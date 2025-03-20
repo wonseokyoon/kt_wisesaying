88bee6cd56dde23890a2b5a8b8b8f533e704bd27
wonseokyoon <01088115030a@gmail.com>
2025-03-20 15:46:47 +0900

    feat: 목록

---
+++ src/main/kotlin/domain/wisesaying/entity/WiseSaying.kt
@@ -0,0 +1,9 @@
+package domain.wisesaying.entity
+
+data class WiseSaying(
+    var id:Int,
+    var saying:String,
+    var author:String
+) {
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,9 @@
+import domain.wisesaying.entity.WiseSaying
 
 fun main(){
     var lastId=0
+    val wiseSayings= mutableListOf<WiseSaying>()
+
     println("== 명언 앱 ==")
     while(true) {
         print("명령: ")
@@ -13,12 +16,17 @@ fun main(){
                 val saying=readlnOrNull() ?: ""
                 print("작가 : ")
                 val author=readlnOrNull() ?: ""
-                lastId++
+                ++lastId
                 println("${lastId}번 명언이 등록되었습니다.")
                 println("[${saying} _ $author]")
+                wiseSayings.add(WiseSaying(lastId, saying, author))
             }
             "목록" -> {
-
+                println("번호 / 작가 / 명언")
+                println("----------------------")
+                wiseSayings.forEach{
+                    println("${it.id} / ${it.author} / ${it.author}")
+                }
             }
 
 
