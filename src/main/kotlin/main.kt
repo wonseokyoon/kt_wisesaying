32158b8e0fba0681a48429590fab7c9c164c9f57
wonseokyoon <01088115030a@gmail.com>
2025-03-20 15:35:02 +0900

    feat:등록

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,27 @@
 
-
 fun main(){
+    var lastId=0
+    println("== 명언 앱 ==")
+    while(true) {
+        print("명령: ")
+        val input= readlnOrNull() ?: ""
 
+        when(input){
+            "종료" -> break
+            "등록" ->{
+                print("명언 : ")
+                val saying=readlnOrNull() ?: ""
+                print("작가 : ")
+                val author=readlnOrNull() ?: ""
+                lastId++
+                println("${lastId}번 명언이 등록되었습니다.")
+                println("[${saying} _ $author]")
+            }
+            "목록" -> {
+
+            }
+
+
+        }
+    }
 }
